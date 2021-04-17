9658f54d8f087d8fd4afa6b3459125c9eeff9e72
hedibenkhouja8 <hedibenkhouja8@outlook.fr>
2021-04-17 15:21:08 +0100

    base de donne

---
+++ app/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //protected $table="esm eli t7eb aalih";
+    //protected $primarykey="esm eli t7eb aalih";
+    //je dois les changer dans la migration
+    // ajouter ->primarykey()
+}
