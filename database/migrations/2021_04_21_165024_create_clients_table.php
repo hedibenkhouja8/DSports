b5c77567249bce452bbe7837bb61b0bb6d087207
Hachaniwassim <80152468+Hachaniwassim@users.noreply.github.com>
2021-04-21 18:00:37 +0100

    wassimcommit

---
+++ database/migrations/2021_04_21_165024_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->integer('age');
+            $table->integer('code_coach')->unsigned();
+            $table->enum('sport',['yoga','danse','box']);
+            $table->string('datedebutabonnement');
+            $table->string('datefinabonnement');
+            $table->timestamps();
+            $table->foreign('code_coach')->references('id')->on('coaches')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
