f91e8f734d791d04282273852ebced1a189dac6c
hedibenkhouja8 <hedibenkhouja8@outlook.fr>
2021-04-24 10:34:58 +0100

    model changes

--- app/Coach.php
+++ app/Coach.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Coach extends Model
 {
-    //
+    public function clients()
+    {
+        return $this->hasMany('App\Client','code_coach');
+    }
 }
