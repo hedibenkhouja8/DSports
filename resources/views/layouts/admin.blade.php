82b6829a6d1316cc2dc0123ca59417f5739b8b05
Hachaniwassim <80152468+Hachaniwassim@users.noreply.github.com>
2021-05-06 16:43:30 +0100

    admin dashboard

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+
+
+
+<table class="table table-striped table-light">
+    <thead class="thead-dark">
+        <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>specialité </th>
+            <th>email</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+
+
+ 
+
+        
+@endsection
+
+
+           
