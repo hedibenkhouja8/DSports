52176722f1f4484aeadca72973ed888b3baceb22
hedibenkhouja8 <hedibenkhouja8@outlook.fr>
2021-05-31 22:23:00 +0100

    everything is done for now

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,8 +66,10 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'role' => $data['role'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+
         ]);
     }
 }
