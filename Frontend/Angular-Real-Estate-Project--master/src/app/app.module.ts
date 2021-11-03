4b4dcd5b32a5d160b572e3b3282fdb29fd4f1c8e
hedibenkhouja8 <hedibenkhouja8@outlook.fr>
2021-11-03 11:46:03 +0100

    first commit

---
+++ Frontend/Angular-Real-Estate-Project--master/src/app/app.module.ts
@@ -0,0 +1,20 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+//import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+@NgModule({
+  declarations: [
+    AppComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
