8c04f493302c40db72652d2e1fcbf0e8a4e1dafb
yousseff650 <yousseffarhat818@gmail.com>
2021-11-07 15:19:53 +0100

    creation de navbar component

--- Frontend/Angular-Real-Estate-Project--master/src/app/app.module.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/app.module.ts
@@ -5,9 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 //import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactusComponent } from './contactus/contactus.component';
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    ContactusComponent,
   
   ],
   imports: [
