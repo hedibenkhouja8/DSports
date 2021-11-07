16dad77b5e097eed0cb84d225e11a171b7b0ada5
labidimalek <melek.islem105@gmail.com>
2021-11-07 16:49:46 +0100

    component services

--- Frontend/Angular-Real-Estate-Project--master/src/app/app.module.ts
+++ Frontend/Angular-Real-Estate-Project--master/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContactusComponent } from './contactus/contactus.component';
 import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ContactusComponent,
     HomeComponent,
+    ServicesComponent,
   
   ],
   imports: [
